c2ac419b9ac7af5f4a5cf2082eea3277fbc53022
Patryk Pater <paterp989@gmail.com>
2018-12-28 17:17:44 +0100

    outer facing contract - first steps

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using Library.API.Helpers;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName}{author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+
+                });
+            }
+
+            return new JsonResult(authors);
+        }
+    }
+}
