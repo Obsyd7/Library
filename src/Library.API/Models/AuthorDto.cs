f9d993f49e8c84f4c9712e9611a3fc64e720f61f
Patryk Pater <paterp989@gmail.com>
2018-12-30 16:12:03 +0100

    author collections

--- src/Library.API/Models/AuthorDto.cs
+++ src/Library.API/Models/AuthorDto.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Library.API.Models
 {
@@ -9,6 +8,5 @@ namespace Library.API.Models
         public string Name { get; set; }
         public int Age { get; set; }
         public string Genre { get; set; }
-        public ICollection<BookForCreationDto> Books { get; set; }
     }
 }
