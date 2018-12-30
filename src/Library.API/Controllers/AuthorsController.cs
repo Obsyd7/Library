9ca278755090003f6a1ef23a15469a7c7a9b1e39
Patryk Pater <paterp989@gmail.com>
2018-12-30 16:22:07 +0100

    appropriate status codes on post t a single resource methods

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -2,6 +2,7 @@
 using Library.API.Entities;
 using Library.API.Models;
 using Library.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -59,6 +60,14 @@ namespace Library.API.Controllers
             return CreatedAtRoute("GetAuthor",
                 new { id = authorToReturn.Id },
                 authorToReturn);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult BlockAuthorCreation(Guid id)
+        {
+            return _libraryRepository.AuthorExists(id) ?
+                new StatusCodeResult(StatusCodes.Status409Conflict) :
+                NotFound();
         }
     }
 }
