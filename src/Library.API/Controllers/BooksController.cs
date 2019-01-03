0c2140e495566e25710f7f696e594a9beb5a7730
Patryk Pater <paterp989@gmail.com>
2019-01-03 12:05:51 +0100

    partial update

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using Library.API.Entities;
 using Library.API.Models;
 using Library.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -102,7 +103,21 @@ namespace Library.API.Controllers
             var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
 
             if (bookForAuthorFromRepo == null)
-                return NotFound();
+            {
+                var bookToAdd = Mapper.Map<Book>(book);
+                bookToAdd.Id = id;
+
+                _libraryRepository.AddBookForAuthor(authorId, bookToAdd);
+
+                if (!_libraryRepository.Save())
+                    throw new Exception($"Upserting book {id} for author {authorId} failed on save.");
+
+                var bookToReturn = Mapper.Map<BookDto>(bookToAdd);
+
+                return CreatedAtRoute("GetBookForAuthor",
+                    new { authorId, id = bookToReturn.Id },
+                    bookToReturn);
+            }
 
             Mapper.Map(book, bookForAuthorFromRepo);
 
@@ -110,6 +125,37 @@ namespace Library.API.Controllers
 
             if (!_libraryRepository.Save())
                 throw new Exception($"Updating book {id} for author {authorId} failed on save.");
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PartiallyUpdateBookForAuthor(Guid authorId, Guid id,
+            [FromBody] JsonPatchDocument<BookForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+                return BadRequest();
+
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+
+            if (bookForAuthorFromRepo == null)
+                return NotFound();
+
+            var bookToPatch = Mapper.Map<BookForUpdateDto>(bookForAuthorFromRepo);
+
+            patchDoc.ApplyTo(bookToPatch);
+
+            //add validation
+
+            Mapper.Map(bookToPatch, bookForAuthorFromRepo);
+
+            _libraryRepository.UpdateBookForAuthor(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+                throw new Exception($"Patching book {id} for author {authorId} failed on save.");
 
             return NoContent();
         }
--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -75,6 +75,7 @@ namespace Library.API
                 cfg.CreateMap<AuthorForCreationDto, Author>();
                 cfg.CreateMap<BookForCreationDto, Book>();
                 cfg.CreateMap<BookForUpdateDto, Book>();
+                cfg.CreateMap<Book, BookForUpdateDto>();
             });
 
             libraryContext.EnsureSeedDataForContext();
