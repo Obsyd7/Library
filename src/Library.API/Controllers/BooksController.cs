c927ff2bdc3dbde1fccd5317c8d69702954481df
Patryk Pater <paterp989@gmail.com>
2019-01-04 11:46:10 +0100

    logging faults

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -160,7 +160,16 @@ namespace Library.API.Controllers
             if (bookForAuthorFromRepo == null)
             {
                 var bookDto = new BookForUpdateDto();
-                patchDoc.ApplyTo(bookDto);
+                patchDoc.ApplyTo(bookDto, ModelState);
+
+                if (bookDto.Description == bookDto.Title)
+                    ModelState.AddModelError(nameof(BookForUpdateDto),
+                        "The provided description should be different from the title");
+
+                TryValidateModel(bookDto);
+
+                if (!ModelState.IsValid)
+                    return new UnprocessableEntityObjectResult(ModelState);
 
                 var bookToAdd = Mapper.Map<Book>(bookDto);
                 bookToAdd.Id = id;
@@ -176,9 +185,18 @@ namespace Library.API.Controllers
 
             var bookToPatch = Mapper.Map<BookForUpdateDto>(bookForAuthorFromRepo);
 
+            // patchDoc.ApplyTo(bookToPatch, ModelState);
+
             patchDoc.ApplyTo(bookToPatch);
 
-            //add validation
+            if (bookToPatch.Description == bookToPatch.Title)
+                ModelState.AddModelError(nameof(BookForUpdateDto),
+                    "The provided description should be different from the title");
+
+            TryValidateModel(bookToPatch);
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
 
             Mapper.Map(bookToPatch, bookForAuthorFromRepo);
 
