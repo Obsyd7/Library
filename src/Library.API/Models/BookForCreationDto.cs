0ec79d8ad11eb9bba23a60e8f5bf2a12007a7f6e
Patryk Pater <paterp989@gmail.com>
2019-01-04 10:09:23 +0100

    post validations

--- src/Library.API/Models/BookForCreationDto.cs
+++ src/Library.API/Models/BookForCreationDto.cs
@@ -1,8 +1,15 @@
-﻿namespace Library.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
 {
     public class BookForCreationDto
     {
+
+        [Required(ErrorMessage = "You should fill out a title")]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "The description shouldn't have more than 500 characters")]
         public string Description { get; set; }
     }
 }
